9b867cfdb69119a5c5cc31eb1b536a1d373ac6ba
janna <jannakondeth5@gmail.com>
2023-11-19 17:18:52 +0530

    second commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function Github(){
+    const[data,setData]=useState([])
+
+    useEffect(()=>{
+       fetch('https://api.github.com/users/Janna007')
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data)
+        setData(data)
+      })
+     },[])
+    return(
+        <>
+         <div className=" text-center  m-4 bg-slate-600 text-white p-4 text-3xl">
+            GitHub Followers :{data.followers}
+            <img  src={data.avatar_url} alt="Image not Found" width={300} />
+         
+         </div>
+        </>
+    )
+}
+
+export default Github
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -72,7 +72,21 @@ export default function Header() {
                                           hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    Contact Us
+                                    Contact 
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to="/github"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 
+                                        ${isActive ? "text-orange-600" :"text-gray-700"} 
+                                        border-b border-gray-100
+                                         hover:bg-gray-50 lg:hover:bg-transparent lg:border-0
+                                          hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    GitHub
                                 </NavLink>
                             </li>
                             
